553c79b502914ae5ba197b5d5f87bc24cf69e98e
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-01-13 20:07:05 +0800

    Initial commit

---
+++ MyCopyMoveRes/CopyMoveDir.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CopyMoveRes
+{
+    public class CopyMoveDir
+    {
+        private const string defaultString = "target = \"\"";
+        private const string configPath = @"Config.txt";
+
+        private string path;
+
+        public CopyMoveDir()
+        {
+            try
+            {
+                if (!File.Exists(configPath))
+                {
+                    CreateDefaultFile();
+                    Console.WriteLine("Create Default File");
+                }
+
+                using (var sr = new StreamReader(configPath))
+                {
+                    var s1 = sr.ReadLine();
+                    var start = s1.IndexOf('"') + 1;
+                    var end = s1.LastIndexOf('"');
+                    path = s1.Substring(start, end - start);
+                    if (string.IsNullOrEmpty(path))
+                    {
+                        Console.WriteLine("Path is null or Empty");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void CopyAndMove()
+        {
+            //清空原来的文件夹 但是保留当前自己
+            if (string.IsNullOrEmpty(path))
+            {
+                Console.WriteLine("Path is null or Empty");
+                return;
+            }
+
+            DirectoryInfo from = new DirectoryInfo(path);
+
+            DirectoryInfo to = from.Parent;
+
+            string fromName = from.Name;
+            string toName = from.Parent.FullName + @"\";
+            //先删除
+            foreach (var item in to.GetDirectories())
+            {
+                if (item.Name != fromName)
+                {
+                    Directory.Delete(item.FullName, true);
+                }
+            }
+            foreach (var item in to.GetFiles())
+            {
+                File.Delete(item.FullName);
+            }
+
+            //后移动
+            foreach (var item in from.GetDirectories())
+            {
+                Console.WriteLine(toName + item.Name);
+
+                Directory.Move(item.FullName, toName + item.Name);
+            }
+            foreach (var item in from.GetFiles())
+            {
+                File.Move(item.FullName, toName+item.Name);
+            }
+        }
+
+        public void CreateDefaultFile()
+        {
+            using (var fs = File.Open(configPath, FileMode.OpenOrCreate))
+            {
+                var bs = Encoding.UTF8.GetBytes(defaultString);
+                fs.Write(bs, 0, bs.Length);
+            }
+        }
+    }
+}
---
+++ MyCopyMoveRes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CopyMoveRes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new CopyMoveDir();
+            program.CopyAndMove();
+
+            Thread.Sleep(5*1000);
+        }
+    }
+}
